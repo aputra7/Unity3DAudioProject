172edfc92320ddcc03f7bf9cc987b16d396f13ac
aputra7 <andikaputra@lawn-128-61-122-104.lawn.gatech.edu>
2014-11-07 11:22:13 -0500

    Added first person controller

---
+++ AudioTour/Assets/Scripts/Camera/CameraPowerSwitch.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPowerSwitch : MonoBehaviour {
+
+	KeyCode key = KeyCode.Escape;
+	Camera mainCamera;
+	string initialGameObjectName;
+
+
+	public bool cameraState
+	{
+		get { return mainCamera.enabled; }
+		set { mainCamera.enabled = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		initialGameObjectName = gameObject.name;
+		gameObject.name = initialGameObjectName + " (On)";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (key))
+		{
+			cameraState = !cameraState;
+			if (cameraState)
+				gameObject.name = initialGameObjectName + " (On)";
+			else
+				gameObject.name = initialGameObjectName + " (Off)";
+		}
+	}
+}
