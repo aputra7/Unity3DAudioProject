803eb7c81e904837e397dab7f4d28599fcdfc99d
aputra7 <andikaputra@Andikas-MacBook-Air.local>
2014-11-13 22:19:32 -0500

    Added eight new shops including audio and material

---
+++ AudioTour/Assets/Project/Scripts/TriggerCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCollider : MonoBehaviour {
+
+	public GameObject otherObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!otherObject.audio.isPlaying) {
+		    otherObject.audio.Play ();
+		}
+	}
+}
