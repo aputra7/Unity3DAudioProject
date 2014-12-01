334db1db8f8da51324769806faee5c11e731093a
aputra7 <andikaputra@Andikas-MacBook-Air.local>
2014-12-01 17:45:31 -0500

    Added ambient sound feature

---
+++ AudioTour/Assets/Project/Scripts/AmbientScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientScript : MonoBehaviour {
+
+	public GameObject otherObject;
+	private bool fadeOut = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fadeOut) {
+			if (otherObject.audio.volume > 0) {
+			    otherObject.audio.volume -= 1 * Time.deltaTime;
+			} else {
+				otherObject.audio.Stop();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		fadeOut = false;
+		otherObject.audio.volume = 1;
+		if (!otherObject.audio.isPlaying) {
+			otherObject.audio.Play ();
+			otherObject.audio.loop = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (otherObject.audio.isPlaying) {
+			fadeOut = true;
+		}
+	}
+}
