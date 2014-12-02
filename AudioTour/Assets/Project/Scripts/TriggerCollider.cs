46978c17c4fcd7fd587556da4e42403bd00e4594
aputra7 <andikaputra@Andikas-MacBook-Air.local>
2014-12-02 17:33:14 -0500

    Integrated ratings sound

--- AudioTour/Assets/Project/Scripts/TriggerCollider.cs
+++ AudioTour/Assets/Project/Scripts/TriggerCollider.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class TriggerCollider : MonoBehaviour {
 
 	public GameObject otherObject;
+	public AudioClip defaultClip;
+	public AudioClip ratings;
+	private bool showRatings = false;
 
 	// Use this for initialization
 	void Start () {
@@ -12,12 +15,18 @@ public class TriggerCollider : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+		if (!otherObject.audio.isPlaying && showRatings) {
+			otherObject.audio.clip = ratings;
+			otherObject.audio.Play();
+			showRatings = false;
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
 		if (!otherObject.audio.isPlaying) {
+			otherObject.audio.clip = defaultClip;
 		    otherObject.audio.Play ();
+			showRatings = true;
 		}
 	}
 }
